d2669cf051a5dadeede389eae3e027ea2850df7e
Siddharth <officialsiddharth.pandey@gmail.com>
2026-03-30 03:50:56 +0530

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Siddharth Pandey on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
